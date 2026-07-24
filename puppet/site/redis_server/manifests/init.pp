862cb61467c1e6ff02d5208470501b5110410dcf
awais <macheema3004@gmail.com>
2026-07-25 01:12:42 +0200

    used pdk to fix redis-server complation and formatting error.

--- puppet/site/redis_server/manifests/init.pp
+++ puppet/site/redis_server/manifests/init.pp
@@ -1,3 +1,19 @@
+# @summary Installs and configures Redis with a Traefik TCP route.
+#
+# @param hostname
+#   DNS hostname used to access Redis.
+#
+# @param bind
+#   IP address on which Redis listens.
+#
+# @param port
+#   Redis TCP port.
+#
+# @param password
+#   Password required for Redis connections.
+#
+# @param traefik_dynamic_config_dir
+#   Directory watched by Traefik for dynamic configuration.
 class redis_server (
   String  $hostname                   = 'redis.macgrant-platform.test',
   String  $bind                       = '127.0.0.1',
@@ -23,9 +39,9 @@ class redis_server (
     group   => 'traefik',
     mode    => '0640',
     content => epp('traefik/tcp_route.yml.epp', {
-      'route_name'  => 'redis',
-      'entry_point' => 'redis',
-      'target'      => "${bind}:${port}",
+        'route_name'  => 'redis',
+        'entry_point' => 'redis',
+        'target'      => "${bind}:${port}",
     }),
     require => [
       Class['redis'],
